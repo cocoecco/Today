f2c7257d14d95e7e01b6d3fade65002bcd35d987
Shachar <cocoecco@yahoo.com>
2015-04-07 21:39:05 -0700

    Initial Commit

---
+++ TasksToday/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TasksToday
+//
+//  Created by Shachar Udi on 4/7/15.
+//  Copyright (c) 2015 Shachar Udi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
